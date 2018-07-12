96fdf43bff5588eb84b70417850008f2d02746b9
vikas.singh06 <vikas.singh06@BLRMNC330699D.ad.infosys.com>
2018-07-12 12:03:39 +0530

    commmit

---
+++ src/main/java/com/example/DataBasedemo/DataBasedemoApplication.java
@@ -0,0 +1,12 @@
+package com.example.DataBasedemo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DataBasedemoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DataBasedemoApplication.class, args);
+	}
+}
