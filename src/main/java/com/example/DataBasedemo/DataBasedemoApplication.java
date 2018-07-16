79088ebd11c0b6c68d99ce67a2c415c9d11d6fd0
vipinkezekedath.k <vipinkezekedath.k@BLRMNC330699D.ad.infosys.com>
2018-07-16 12:28:06 +0530

    add new sysout

--- src/main/java/com/example/DataBasedemo/DataBasedemoApplication.java
+++ src/main/java/com/example/DataBasedemo/DataBasedemoApplication.java
@@ -10,5 +10,6 @@ public class DataBasedemoApplication {
 		SpringApplication.run(DataBasedemoApplication.class, args);
 		
 		System.out.println("This is for new Barnch Test Working");
+		System.out.println("added by vipin");
 	}
 }
